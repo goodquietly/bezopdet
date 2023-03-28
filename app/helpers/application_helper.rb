63bec32930515520a5453d1e76170c366c07afce
Aleksei Iurin <19alex85@gmail.com>
2023-03-28 15:00:51 +0300

    added simple_calendar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def child_protection(programs)
     programs.where(completed: true).size * 100 / programs.size
   end
+
+  def random_bootstrap_colors
+    %w[primary success danger warning info].sample
+  end
+
+  def left_square_icon
+    content_tag 'i', '', class: 'text-warning bi bi-arrow-left-square-fill'
+  end
+
+  def right_square_icon
+    content_tag 'i', '', class: 'text-warning bi bi-arrow-right-square-fill'
+  end
 end
