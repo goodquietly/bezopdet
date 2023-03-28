50baee19963b163b5f69c1425cad8263a8e1f4a4
Aleksei Iurin <19alex85@gmail.com>
2023-03-29 00:07:37 +0300

    add images to project and many style edits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,35 @@ module ApplicationHelper
     programs.where(completed: true).size * 100 / programs.size
   end
 
+  def protection_color(programs)
+    if child_protection(programs) < 30
+      '#ff4d4d'
+    elsif child_protection(programs) >= 70
+      '#41ff30'
+    else
+      '#eaff30'
+    end
+  end
+
+  def protection_raiting(programs)
+    if child_protection(programs) < 30
+      'НИЗКИЙ'
+    elsif child_protection(programs) >= 70
+      'ВЫСОКИЙ'
+    else
+      'СРЕДНИЙ'
+    end
+  end
+
   def random_bootstrap_colors
     %w[primary success danger warning info].sample
   end
 
   def left_square_icon
-    content_tag 'i', '', class: 'text-warning bi bi-arrow-left-square-fill'
+    content_tag 'i', '', class: 'text-warning bi bi-arrow-left-square-fill fs-4'
   end
 
   def right_square_icon
-    content_tag 'i', '', class: 'text-warning bi bi-arrow-right-square-fill'
+    content_tag 'i', '', class: 'text-warning bi bi-arrow-right-square-fill fs-4'
   end
 end
