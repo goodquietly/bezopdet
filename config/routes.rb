f13376385e1a294fc7de5a620360210a7405fb47
Aleksei Iurin <19alex85@gmail.com>
2023-03-29 17:45:20 +0300

    add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root 'users#show'
+  get '/pages/:page' => 'pages#show'
+
+  root 'pages#show', page: 'home'
 
   devise_for :users
 
