d3b4d16e9bb4bd5a99d0f973bb345c72fe567cc0
Aleksei Iurin <19alex85@gmail.com>
2023-03-29 19:13:03 +0300

    check users controller bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @user = authorize User.find(params[:id])
     @user_programs = policy_scope(UserProgram)
     start_date = params.fetch(:start_date, Date.today).to_date
-    @notices = UserProgram.where(notice_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @notices = @user_programs
+               .where(notice_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 end
