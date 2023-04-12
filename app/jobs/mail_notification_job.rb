2bf73bdcc7f1214192d3393729371d9cfa8d3bdb
Aleksei Iurin <19alex85@gmail.com>
2023-04-12 21:23:55 +0300

    add sidekiq to the job

--- app/jobs/mail_notification_job.rb
+++ app/jobs/mail_notification_job.rb
@@ -1,9 +1,12 @@
-class MailNotificationJob < ApplicationJob
-  queue_as :default
+class MailNotificationJob
+  include Sidekiq::Job
 
-  def perform(model)
-    return UserMailer.new_training_program(model).deliver_later if model.instance_of?(User)
+  def perform(*_args)
+    UserProgram.find_each do |program|
+      # program.update_columns(completed: false, notice_time: nil) # 2 - 3 months
+      # UserMailer.repeat_training_program(program).deliver_later # 2 - 3 months
 
-    UserMailer.notice_time(model).deliver_later
+      # UserMailer.notice_time(program).deliver_later # 3
+    end
   end
 end
