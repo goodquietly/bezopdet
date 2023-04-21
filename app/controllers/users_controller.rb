95733fca0cc8eff90bd77a56a5bf4f8472dda5b8
Aleksei Iurin <19alex85@gmail.com>
2023-04-21 15:25:27 +0300

    add grover and puppeteer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,25 @@ class UsersController < ApplicationController
                .where(notice_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 
-  def passport; end
+  def passport
+    respond_to do |format|
+      format.html
+
+      format.pdf do
+        html_string = ActionController::Base.new.render_to_string(
+          {
+            template: 'users/passport',
+            layout: 'pdf',
+            assigns: { user: @user }
+          }
+        )
+
+        pdf = Grover.new(html_string).to_pdf
+
+        send_data(pdf, filename: "Passport_#{@user.full_name}_#{Date.current}", type: 'application/pdf')
+      end
+    end
+  end
 
   private
 
