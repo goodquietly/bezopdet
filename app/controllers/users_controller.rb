dfbcf81ad2b161e52089ae55c33e5a37fba858ed
Aleksei Iurin <19alex85@gmail.com>
2023-04-21 22:44:51 +0300

    edit grover

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user
+  before_action :set_user_and_programs
 
   def show
-    @user_programs = policy_scope(UserProgram)
     start_date = params.fetch(:start_date, Date.today).to_date
     @notices = @user_programs
                .where(notice_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
@@ -11,17 +10,18 @@ class UsersController < ApplicationController
   def passport
     respond_to do |format|
       format.html
-
       format.pdf do
-        html_string = ActionController::Base.new.render_to_string(
+        relative_html = UsersController.new.render_to_string(
           {
             template: 'users/passport',
             layout: 'pdf',
-            assigns: { user: @user }
+            assigns: { user: @user,
+                       user_programs: @user_programs }
           }
         )
 
-        pdf = Grover.new(html_string).to_pdf
+        absolute_html = Grover::HTMLPreprocessor.process relative_html, 'http://localhost:3000/', 'http'
+        pdf = Grover.new(absolute_html).to_pdf
 
         send_data(pdf, filename: "Passport_#{@user.full_name}_#{Date.current}", type: 'application/pdf')
       end
@@ -30,7 +30,8 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
+  def set_user_and_programs
     @user = authorize User.find(params[:id])
+    @user_programs = policy_scope(UserProgram)
   end
 end
