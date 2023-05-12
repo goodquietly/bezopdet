590686eb3294cc32860032af075655c3cc539104
Aleksei Iurin <19alex85@gmail.com>
2023-05-12 09:42:42 +0300

    CP controller edit and CP views

--- app/controllers/child_programs_controller.rb
+++ app/controllers/child_programs_controller.rb
@@ -4,11 +4,11 @@ class ChildProgramsController < ApplicationController
   def edit; end
 
   def update
-    if @child_program.update(child_program_params)
-      UserMailer.notice_time(@child_program).deliver_later
+    child_program_params = params.require(:child_program).permit(:notice_time)
 
+    if @child_program.update(child_program_params)
       redirect_to child_program_path(@child_program),
-                  notice: 'Напоминание установлено. Вы получите email-уведомление в день события.'
+                  notice: 'Напоминание установлено! Вы получите email-уведомление в день события.'
     else
       render :edit
     end
@@ -19,8 +19,6 @@ class ChildProgramsController < ApplicationController
   def complete
     @child_program.update_columns(completed: true, notice_time: nil, completed_at: Time.now)
 
-    UserMailer.repeat_training_program(@child_program).deliver_later
-
     redirect_to child_program_path(@child_program),
                 notice: 'Методика пройдена! Поздравляем! Рекомендуем повторно пройти методику через 3 месяца.'
   end
@@ -29,9 +27,5 @@ class ChildProgramsController < ApplicationController
 
   def set_child_program
     @child_program ||= authorize ChildProgram.find(params[:id])
-  end
-
-  def child_program_params
-    params.require(:child_program).permit(:notice_time)
   end
 end
