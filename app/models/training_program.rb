57e4f4532dcce48601fb1440bdfa95e9d660c3c7
Aleksei Iurin <19alex85@gmail.com>
2024-03-15 14:23:04 +0300

    add sorted programs at children show page

--- app/models/training_program.rb
+++ app/models/training_program.rb
@@ -8,7 +8,7 @@ class TrainingProgram < ApplicationRecord
   validates :title, presence: true, uniqueness: true, length: { in: 3..500 }
   validates :published, inclusion: [true, false]
   validates :pdf, attached: true, content_type: %i[pdf jpg jpeg],
-                  size: { between: 1.kilobyte..5.megabytes }
+                  size: { between: 1.kilobyte..10.megabytes }
 
   scope :published, -> { where(published: true) }
   scope :unpublished, -> { where(published: false) }
